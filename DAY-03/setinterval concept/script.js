92287579ae63a9e53f25d6eba1687a24b3234ed4
Nileshnhe <nehenilesh111@gmail.com>
2025-11-23 00:49:52 +0530

    small change

--- DAY-03/setinterval concept/script.js
+++ DAY-03/setinterval concept/script.js
@@ -10,3 +10,18 @@ setTimeout(function(){
     clearInterval(int)
 
 },5000)
+
+
+
+
+
+
+
+
+// echo "# JS-Projects" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Nileshnehe/JS-Projects.git
+// git push -u origin main
